ef1cc9e8fb8597d1122bddbf2ee61a070dbfcf6f
Zet-Warden <74450482+Zet-Warden@users.noreply.github.com>
2021-09-30 21:29:49 +0800

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const app = require('express')();
 require('dotenv').config();
 
 app.get('/', (req, res) => {
-    res.send('hello mga sirs');
+    res.send('dingledong');
 });
 
 const port = process.env.PORT || 3000;
