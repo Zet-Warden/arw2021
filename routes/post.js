cdf03a18ec1a4bae09800c3a7d45b3b2289abcec
Zet-Warden <74450482+Zet-Warden@users.noreply.github.com>
2021-10-01 17:08:06 +0800

    Update post.js

--- routes/post.js
+++ routes/post.js
@@ -1,31 +1,33 @@
-const express = require('express');
-const db = require('mongoose');
-const Feedback = require('../model/feedback');
-const Question = require('../model/question');
-require('dotenv').config();
+const express = require("express");
+const db = require("mongoose");
+const Feedback = require("../model/feedback");
+const Question = require("../model/question");
+require("dotenv").config();
 
 const app = express.Router();
 db.connect(process.env.DB_URI);
 
-app.post('/questions', (req, res) => {
+app.post("/questions", (req, res) => {
+    res.set("Access-Control-Allow-Origin", "*");
     const result = new Question(req.body).save();
+
     result
         .then(() => {
-            res.status(200).json({ status: 'Okay', msg: 'Data saved' });
+            res.status(200).json({ status: "Okay", msg: "Data saved" });
         })
         .catch(() => {
-            res.status(500).json({ status: 'Error', msg: 'Data not saved' });
+            res.status(500).json({ status: "Error", msg: "Data not saved" });
         });
 });
 
-app.post('/feedbacks', (req, res) => {
+app.post("/feedbacks", (req, res) => {
     const result = new Feedback(req.body).save();
     result
         .then(() => {
-            res.status(200).json({ status: 'Okay', msg: 'Data saved' });
+            res.status(200).json({ status: "Okay", msg: "Data saved" });
         })
         .catch(() => {
-            res.status(500).json({ status: 'Error', msg: 'Data not saved' });
+            res.status(500).json({ status: "Error", msg: "Data not saved" });
         });
 });
 
