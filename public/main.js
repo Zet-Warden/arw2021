ba31ef40fde3ccf392d828e06c57ae545fc3061a
Zet-Warden <74450482+Zet-Warden@users.noreply.github.com>
2021-10-01 15:54:58 +0800

    improved ui

--- public/main.js
+++ public/main.js
@@ -1,6 +1,10 @@
+const questionTable = document.querySelector(".table.question");
+const feedbackTable = document.querySelector(".table.feedback");
+
 const rows = document.querySelectorAll(".row.content");
 const checkboxes = document.querySelectorAll('input[type="checkbox"]');
 const ids = document.querySelectorAll(".id");
+const navBtns = document.querySelectorAll("li");
 
 checkboxes.forEach((cb, index) => {
     cb.addEventListener("click", (event) => {
@@ -22,3 +26,24 @@ checkboxes.forEach((cb, index) => {
         });
     });
 });
+
+navBtns.forEach((btn, index) => {
+    btn.addEventListener("click", () => {
+        resetAllButtons();
+        btn.classList.toggle("active");
+        switch (index) {
+            case 0:
+                questionTable.classList.remove("invisible");
+                feedbackTable.classList.add("invisible");
+                break;
+            case 1:
+                feedbackTable.classList.remove("invisible");
+                questionTable.classList.add("invisible");
+                break;
+        }
+    });
+});
+
+function resetAllButtons() {
+    navBtns.forEach((btn) => btn.classList.remove("active"));
+}
